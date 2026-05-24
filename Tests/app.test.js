e239390eef4eed61956d2c657fe21e8178d39d5c
Leladeira <lgkcladeira@gmail.com>
2026-05-24 10:53:49 -0300

    feat: atualiza endpoint de health check e assercoes do teste

--- Tests/app.test.js
+++ Tests/app.test.js
@@ -1,9 +1,13 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../app'); 
 
 describe('Testes de Integração da API', () => {
   it('Deve retornar status 200 e objeto "Fase1 Devops - Entregue" na rota de health', async () => {
     const res = await request(app).get('/api/v1/health');
+    
+    console.log('Dados recebidos do Endpoint:', res.body);
+    console.log('Status HTTP recebido:', res.statusCode);
+
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty('status', 'Fase1 Devops - Entregue');
   });
--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 
 app.use(express.json());
 
-// Rota de Health Check atualizada com a sua nova mensagem
 app.get('/api/v1/health', (req, res) => {
   res.status(200).json({ status: 'Fase1 Devops - Entregue' });
 });
