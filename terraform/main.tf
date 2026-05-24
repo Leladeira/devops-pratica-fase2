6594c82ab91726a5fd8642db00114abdd16b00b4
Leladeira <lgkcladeira@gmail.com>
2026-05-24 16:40:38 -0300

    fix: distribui subnets do terraform em diferentes AZs para suporte ao RDS

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-variable "region" {
+vvariable "region" {
   description = "Regiao da AWS"
   default     = "us-east-1"
 }
@@ -9,34 +9,26 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "devops-vpc"
-  }
+  tags = { Name = "devops-vpc" }
 }
 
-resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "${var.region}a"
-  tags = {
-    Name = "devops-public-subnet"
-  }
+  availability_zone       = "${var.region}a"
+  tags = { Name = "devops-public-subnet-a" }
 }
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private_b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "${var.region}a"
-  tags = {
-    Name = "devops-private-subnet"
-  }
+  availability_zone = "${var.region}b"
+  tags = { Name = "devops-private-subnet-b" }
 }
 
 resource "aws_db_subnet_group" "db_group" {
   name       = "devops-db-subnet-group"
-  subnet_ids = [aws_subnet.public.id, aws_subnet.private.id]
-  tags = {
-    Name = "devops-db-subnet-group"
-  }
+  subnet_ids = [aws_subnet.public_a.id, aws_subnet.private_b.id]
+  tags       = { Name = "devops-db-subnet-group" }
 }
